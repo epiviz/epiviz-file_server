05324142e0d8ed2793109b10d47a7c068775228d
Hector Corrada Bravo <hcorrada@gmail.com>
2015-09-09 12:09:42 -0400

    first cut

--- frontend/epiviz/websocket/EpiVizPy.py
+++ frontend/epiviz/websocket/EpiVizPy.py
@@ -19,38 +19,34 @@ class EpiVizPy(object):
     classdocs
     '''
 
-    def __init__(self, console_listener=None, server_path=r'/ws'):
+    def __init__(self, console_listener=None, server_path=r'/ws', rserve_host = 'localhost', rserve_port = 6311):
         '''
         Constructor
         '''
-        measurements = []
-        measurements_lock = Lock()
+        # start Rserve connection
+        self._rserve_conn = pyRserve.connect(host=rserve_host, port=rserve_port)
 
-        # Append a mock measurement
-        measurements.append({
-          'id': 'py_column',
-          'name': 'Python Measurement',
-          'type': 'feature',
-          'datasourceId': 'py_datasource',
-          'datasourceGroup': 'py_datasourcegroup',
-          'defaultChartType': 'Line Track',
-          'annotation': None,
-          'minValue':-5,
-          'maxValue': 25,
-          'metadata': ['py_metadata']
-        })
-
+        # define the handler function
+        self._rserve_conn.voidEval("""handle_request <- function(id, action, msgData)
+                                   {
+                                     out <- list(type="response",
+                                                 requestId=id,
+                                                 data=NULL);
+                                     out$data <- mgr$handle(action, msgData);
+                                     epivizr:::toJSON(out)
+                                  }""")
+        self._handler = self._rserve_conn.r.handle_request
         self._thread = None
         self._server = None
         self._console_listener = console_listener
         self._application = tornado.web.Application([(server_path, EpiVizPyEndpoint, {
           'console_listener': console_listener,
-          'measurements': measurements,
-          'measurements_lock': measurements_lock
+          'handler': self.handler
         })])
 
     def start(self, port=8888):
         self.stop()
+
         self._thread = threading.Thread(target=lambda: self._listen(port)).start()
         if not self._console_listener is None:
             self._console_listener.listen()
