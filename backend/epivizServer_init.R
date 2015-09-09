5e185aedd2096f12b5328d8d1c2effce6875fce8
Hector Corrada Bravo <hcorrada@gmail.com>
2015-09-09 12:50:27 -0400

    prototype using example bed file

--- backend/epivizServer_init.R
+++ backend/epivizServer_init.R
@@ -2,5 +2,5 @@ library(epivizr)
 
 mgr = epivizr:::EpivizDeviceMgr$new()
 
-data(tcga_colon_example)
-colon_blocks_ms <- mgr$addMeasurements(colon_blocks, "450k colon blocks")
+colon_blocks <- rtracklayer::import("example_data/colon_blocks.bed")
+mgr$addMeasurements(colon_blocks, "450k colon blocks")
