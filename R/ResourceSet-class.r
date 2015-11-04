8101b5e4174c7319f91190e3d0a29b98ec60d3af
Hector Corrada Bravo <hcorrada@gmail.com>
2015-11-04 10:42:05 -0500

    start yaml resource parser

--- R/ResourceSet-class.r
+++ R/ResourceSet-class.r
@@ -11,6 +11,47 @@ setMethod("show", "ResourceSet",
                   "  ", length(object@resources), " resources\n"))
          })
 
+setClass("Resource",
+         representation(
+           name="character",
+           dataObject="ANY",
+           record="list"
+         ))
+
+setMethod("show", "Resource",
+          function(object) {
+            cat(paste0("Resource <", object@name, ">:\n",
+                       " data class: ", class(object@dataObject)))
+          })
+
+.createResource <- function(name, record) {
+  path <- record$url
+  format <- tools::file_ext(path)
+
+  if (!is.null(record$filetype)) {
+    format <- record$filetype
+  }
+  object <- rtracklayer::FileForFormat(path, format)
+  columns <- record$columns
+  datatype <- record$datatype
+  new("Resource", name=name, dataObject=object, record=record)
+}
+
+.parseResources_yaml <- function(filepath) {
+  resourceRecords <- yaml::yaml.load_file(filepath)
+  mapply(.createResource, name=names(resourceRecords), record=resourceRecords)
+}
+
+.parseResources <- function(sourceString) {
+  # parse based on filetype
+  .parser <- switch(tools::file_ext(sourceString),
+                    yml=.parseResources_yaml,
+                    yaml=.parseResources_yaml,
+                    .parseResources_path)
+  .parser(sourceString)
+}
+
 ResourceSet <- function(sourceString) {
-  new("ResourceSet", sourceString=sourceString, resources=list())
+  resources <- .parseResources(sourceString)
+  new("ResourceSet", sourceString=sourceString, resources=resources)
 }
